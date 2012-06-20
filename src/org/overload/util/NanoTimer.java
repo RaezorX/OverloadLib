c2e38841abaf34268464c9637cd92fa3d9044017
Bradley Odell <btodell@hotmail.com>
2012-06-20 08:53:08 -0700

    finished up the documentation that i wasn't able to last night :/

--- src/org/overload/util/NanoTimer.java
+++ src/org/overload/util/NanoTimer.java
@@ -6,71 +6,96 @@ package org.overload.util;
  */
 public class NanoTimer {
 	
-	private Long start = null, target = null, pause = null;
+	private Long start = null, timeout = null, pause = null;
 	
 	private NanoTimer() {
 		reset();
 	}
 	
-	private NanoTimer(final long elapseTarget) {
-		target = elapseTarget;
+	private NanoTimer(final long elapseTimeout) {
+		timeout = elapseTimeout;
 		reset();
 	}
 	
 	/**
-	 * @return whether this timer is active.
+	 * @return whether this timer hasn't passed its elapsed timeout.
 	 */
 	public boolean isActive() {
-		return !isPaused() && (target == null || getElapsedNanos() < target);
+		return !isPaused() && (timeout == null || getElapsedNanos() < timeout);
 	}
 	
+	/**
+	 * @return if this timer has been paused.
+	 */
 	public boolean isPaused() {
 		return pause != null;
 	}
 	
-	public void appendNanos(final long nanos) {
-		start += nanos;
-	}
-	
-	public void appendMillis(final long millis) {
-		appendNanos(millis * 1000000L);
-	}
-	
+	/**
+	 * Resets this timer so the elapsed time starts over at 0.
+	 */
 	public void reset() {
 		start = System.nanoTime();
 	}
 	
+	/**
+	 * Pauses this timer causing the elapsed time to stop.
+	 */
 	public void pause() {
 		if (pause == null) {
 			pause = System.nanoTime();
 		}
 	}
 	
+	/**
+	 * Resumes the timer, elapsed time will continue to increase.
+	 */
 	public void resume() {
 		if (pause != null) {
-			appendNanos(System.nanoTime() - pause);
+			start += System.nanoTime() - pause;
 			pause = null;
 		}
 	}
 	
+	/**
+	 * @return the count of nanoseconds that have elapsed since the timer start or a reset.
+	 */
 	public long getElapsedNanos() {
 		return System.nanoTime() - start;
 	}
 	
+	/**
+	 * @return elapsed milliseconds in double precision.
+	 */
 	public double getElapsedMillis() {
 		return ((double) getElapsedNanos()) / 1000000.D;
 	}
 	
+	/**
+	 * @return a plain NanoTimer with no elapsed timeout.
+	 */
 	public static NanoTimer newInstance() {
 		return new NanoTimer();
 	}
 	
-	public static NanoTimer newInstanceNano(final long elapseTarget) {
-		return new NanoTimer(elapseTarget);
+	/**
+	 * Creates a new NanoTimer with the given elapsed timeout in nanoseconds.
+	 * @param elapseTimeout
+	 * 		how many nanoseconds are to elapse before this timer is considered inactive.
+	 * @return the new NanoTimer.
+	 */
+	public static NanoTimer newInstanceNano(final long elapseTimeout) {
+		return new NanoTimer(elapseTimeout);
 	}
 	
-	public static NanoTimer newInstanceMillis(final long elapseTarget) {
-		return new NanoTimer(elapseTarget * 1000000L);
+	/**
+	 * Creates a new NanoTimer with the given elapsed timeout in milliseconds.
+	 * @param elapseTimeout
+	 * 		how many milliseconds are to elapse before this timer is considered inactive.
+	 * @return the new NanoTimer.
+	 */
+	public static NanoTimer newInstanceMillis(final long elapseTimeout) {
+		return new NanoTimer(elapseTimeout * 1000000L);
 	}
 	
 }
